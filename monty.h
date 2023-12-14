86254537ff8f472e14b1f9e21e83e22546c50892
Michael Mwangi <michaelmichire2016@gmail.com>
2023-12-14 00:03:47 -0800

    The main header filee for the project

--- monty.h
+++ monty.h
@@ -1,6 +1,14 @@
 #ifndef MONTY_H
 #define MONTY_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
+#include <sys/types.h>
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
