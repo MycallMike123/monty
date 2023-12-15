666ec09eb0edf8d2102337731aa7e093d6ebfd12
Michael Mwangi <michaelmichire2016@gmail.com>
2023-12-15 07:06:54 -0800

    A fuction that handles adding, printing, removing and swapping elements of stack

--- _push.c
+++ _push.c
@@ -3,21 +3,72 @@
 
 /**
  * handle_push - Adds a node to the stack
- * @n: Number to go inside of the node
- * Return: A pointer to the node
+ * @head: Pointer to the head of the stack
+ * @counter: Line number in the Monty bytecode file
 */
 
-stack_t *handle_push(int n)
+void handle_push(stack_t **head, unsigned int counter)
 {
-	stack_t *n_node;
+	int value, b, flag;
 
-	n_node = malloc(sizeof(stack_t));
-	if (n_node == NULL)
-		error_(4);
-	n_node->next = NULL;
-	n_node->prev = NULL;
-	n_node->n = n;
-	return (n_node);
+	b = 0;
+	flag = 0;
+	if (line.arg)
+	{
+		if (line.arg[0] == '-')
+			b++;
+		for (; line.arg[b] != '\0'; b++)
+		{
+			if (line.arg[b] > 57 || line.arg[b] < 48)
+				flag = 1;
+		}
+		if (flag == 1)
+		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+			fclose(line.file);
+			free(line.cont);
+			f_stack(*head);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+		fclose(line.file);
+		free(line.cont);
+		f_stack(*head);
+		exit(EXIT_FAILURE); }
+	value = atoi(line.arg);
+
+	if (line.flag_c == 0)
+	{
+		_addnode(head, value);
+	}
+	else
+	{
+		_addqueue(head, value);
+	}
+}
+
+/**
+ * handle_pall - Prints the elements of the stack
+ * @head: Pointer to the head of the stack
+ * @counter: Unused parameter
+ */
+
+void handle_pall(stack_t **head, unsigned int counter)
+{
+	stack_t *current;
+	(void)counter;
+
+	current = *head;
+	if (current == NULL)
+	{
+		return;
+	}
+	while (current)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+	}
 }
 
 /**
@@ -28,8 +79,14 @@ stack_t *handle_push(int n)
 
 void handle_pint(stack_t **head, unsigned int counter)
 {
-	if (head == NULL || *head == NULL)
-		_error(6, counter);
+	if (*head == NULL)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", counter);
+		fclose(line.file);
+		free(line.cont);
+		f_stack(*head);
+		exit(EXIT_FAILURE);
+	}
 	printf("%d\n", (*head)->n);
 }
 
@@ -41,16 +98,19 @@ void handle_pint(stack_t **head, unsigned int counter)
 
 void handle_pop(stack_t **head, unsigned int counter)
 {
-	stack_t *res;
+	stack_t *temp;
 
-	if (head == NULL || *head == NULL)
-		_error(7, counter);
-
-	res = *head;
-	*head = res->next;
-	if (*head != NULL)
-		(*head)->prev = NULL;
-	free(res);
+	if (*head == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", counter);
+		fclose(line.file);
+		free(line.cont);
+		f_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	temp = *head;
+	*head = temp->next;
+	free(temp);
 }
 
 /**
@@ -61,36 +121,26 @@ void handle_pop(stack_t **head, unsigned int counter)
 
 void handle_swap(stack_t **head, unsigned int counter)
 {
-	stack_t *res;
+	stack_t *current;
+	int len, temp;
 
-	if (head == NULL || *head == NULL || (*head)->next == NULL)
+	len = 0;
+	current = *head;
+	while (current)
 	{
-		_error(8, counter, "swap");
+		current = current->next;
+		len++;
 	}
-	res = (*head)->next;
-	(*head)->next = res->next;
-	if (res->next != NULL)
+	if (len < 2)
 	{
-		res->next->prev = *head;
+		fprintf(stderr, "L%d: can't swap, stack too short\n", counter);
+		fclose(line.file);
+		free(line.cont);
+		f_stack(*head);
+		exit(EXIT_FAILURE);
 	}
-	res->next = *head;
-	(*head)->prev = res;
-	res->prev = NULL;
-	*head = res;
-}
-
-/**
- * f_open - Opens a file
- * @file_name: the file namepath
- */
-
-void f_open(char *file_name)
-{
-	FILE *file_descriptor = fopen(file_name, "r");
-
-	if (file_name == NULL || file_descriptor == NULL)
-		error_(2, file_name);
-
-	rd_file(file_descriptor);
-	fclose(file_descriptor);
+	current = *head;
+	temp = current->n;
+	current->n = current->next->n;
+	current->next->n = temp;
 }
