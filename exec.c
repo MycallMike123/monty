cce194103ba2277b246465604f6c2ca39433aefd
Michael Mwangi <michaelmichire2016@gmail.com>
2023-12-15 06:38:30 -0800

    A fuction that handles execution and freeing the stack/the doubly linked list and errors

--- exec.c
+++ exec.c
@@ -2,170 +2,125 @@
 
 /**
 * exec - Executes the Monty opcode
-* @opcode: Pointer to the head of the stack
-* @value: Line content containing the Monty instruction
-* @type: The storage
-* @n: Line number in the Monty bytecode file
+* @stack: Pointer to the head of the stack
+* @counter: Line content containing the Monty instruction
+* @file: Pointer to the Monty bytecode file
+* @cont: Line number in the Monty bytecode file
+* Return: 0 Success, 1 otherwise
 */
 
-void exec(char *opcode, char *value, int n, int type)
+int exec(char *cont, stack_t **stack, unsigned int counter, FILE *file)
 {
-	int a, fl;
+        instruction_t opst[] = {
+                                {"push", handle_push}, {"pall", handle_pall}, {"pint", handle_pint},
+                                {"pop", handle_pop},
+                                {"swap", handle_swap},
+                                {"add", add},
+                                {"nop", handle_nop},
+                                {"sub", sub},
+                                {"div", _div},
+                                {"mul", mul},
+                                {"mod", mod},
+                                {"pchar", pchar},
+                                {"pstr", pstr},
+                                {"rotl", rotl},
+                                {"rotr", rotr},
+                                {"queue", _queue},
+                                {"stack", _stack},
+                                {NULL, NULL}
+                                };
+        unsigned int a = 0;
+        char *op_;
 
-	instruction_t opst[] = {
-				{"push", _addnode}, {"pall", _stack}, {"pint", handle_pint},
-				{"pop", handle_pop},
-				{"swap", handle_swap},
-				{"add", add},
-				{"nop", handle_nop},
-				{"sub", sub},
-				{"div", _div},
-				{"mul", mul},
-				{"mod", mod},
-				{"pchar", pchar},
-				{"pstr", pstr},
-				{"rotl", rotl},
-				{"rotr", rotr},
-				{NULL, NULL}
-				};
-
-	if (opcode[0] == '#')
-	{
-		return;
-	}
-
-	for (fl = 1, a = 0;  opst[a].opcode != NULL; a++)
-	{
-		if (strcmp(opcode, opst[a].opcode) == 0)
-		{
-			f_call(opst[a].f, opcode, value, n, type);
-			fl = 0;
-		}
-	}
-	if (fl == 1)
-		error_(3, n, opcode);
+        op_ = strtok(cont, " \n\t");
+        if (op_ && op_[0] == '#')
+                return (0);
+        line.arg = strtok(NULL, " \n\t");
+        while (opst[a].opcode && op_)
+        {
+                if (strcmp(op_, opst[a].opcode) == 0)
+                {       opst[a].f(stack, counter);
+                        return (0);
+                }
+                a++;
+        }
+        if (op_ && opst[a].opcode == NULL)
+        { fprintf(stderr, "L%d: unknown instruction %s\n", counter, op_);
+                fclose(file);
+                free(cont);
+                f_stack(*stack);
+                exit(EXIT_FAILURE); }
+        return (1);
 }
 
 /**
- * f_stack - Frees nodes in the stack.
- */
-void f_stack(void)
+* f_stack - Frees a doubly linked list
+* @head: Pointer to the head of the stack
+*/
+
+void f_stack(stack_t *head)
 {
-	stack_t *res;
+        stack_t *res;
 
-	if (first == NULL)
-	{
-		return;
-	}
-
-	while (first != NULL)
-	{
-		res = first;
-		first = first->next;
-		free(res);
-	}
+        res = head;
+        while (head)
+        {
+                res = head->next;
+                free(head);
+                head = res;
+        }
 }
 
 /**
- * error_ - Prints appropiate error messages determined by their error code.
- * @error_code: The error codes 1, 2,3,4,5,6,7, and 8
+ * custom_getline - reads a line from a file stream
+ * @content: pointer to the buffer where the line will be stored
+ * @size: pointer to the size of the buffer
+ * @file: file stream to read from
+ * Return: the number of characters read (including newline), or -1 on failure
  */
-
-void error_(int error_code, ...)
+ssize_t custom_getline(char **content, size_t *size, FILE *file)
 {
-	va_list argument;
-	char *op_code;
-	int dig;
+        char *buffer = NULL;
+        size_t bufsize = 0;
+        ssize_t nread = 0;
+        int c;
 
-	va_start(argument, error_code);
-	switch (error_code)
-	{
-		case 1:
-			fprintf(stderr, "USAGE: monty file\n");
-			break;
-		case 2:
-			fprintf(stderr, "Error: Can't open file %s\n",
-				va_arg(argument, char *));
-			break;
-		case 3:
-			dig = va_arg(argument, int);
-			op_code = va_arg(argument, char *);
-			fprintf(stderr, "L%d: unknown instruction %s\n", dig, op_code);
-			break;
-		case 4:
-			fprintf(stderr, "Error: malloc failed\n");
-			break;
-		case 5:
-			fprintf(stderr, "L%d: usage: push integer\n", va_arg(argument, int));
-			break;
-		default:
-			break;
-	}
-	f_stack();
-	exit(EXIT_FAILURE);
-}
+        if (!content || !size || !file)
+                return (-1);
+        if (!*content)
+        { buffer = malloc(BUFFER_SIZE);
+                if (!buffer)
+                        return (-1);
+                bufsize = BUFFER_SIZE; }
+                else
+                { buffer = *content;
+                        bufsize = *size; }
 
-/**
- * _error - handles errors.
- * @error_code: The error codes are 6 to nine
- */
+        while (1)
+        {
+                c = fgetc(file);
 
-void _error(int error_code, ...)
-{
-	va_list argument;
-	char *op_code;
-	int dig;
+                if (c == EOF)
+                {
+                        if (nread == 0)
+                                return (-1); /* No characters read */
+                        break;
+                }
 
-	va_start(argument, error_code);
-	switch (error_code)
-	{
-		case 6:
-			fprintf(stderr, "L%d: can't pint, stack empty\n",
-				va_arg(argument, int));
-			break;
-		case 7:
-			fprintf(stderr, "L%d: can't pop an empty stack\n",
-				va_arg(argument, int));
-			break;
-		case 8:
-			dig = va_arg(argument, unsigned int);
-			op_code = va_arg(argument, char *);
-			fprintf(stderr, "L%d: can't %s, stack too short\n", dig, op_code);
-			break;
-		case 9:
-			fprintf(stderr, "L%d: division by zero\n",
-				va_arg(argument, unsigned int));
-			break;
-		default:
-			break;
-	}
-	f_stack();
-	exit(EXIT_FAILURE);
-}
+                if ((size_t)nread >= bufsize - 1)
+                {
+                        bufsize += BUFFER_SIZE;
+                        buffer = realloc(buffer, bufsize);
+                        if (!buffer)
+                                return (-1);
+                }
 
-/**
- * str_error - handles errors error in strings
- * @error_code: The error codes are 10 and 11
- */
-
-void str_error(int error_code, ...)
-{
-	va_list argument;
-	int dig;
-
-	va_start(argument, error_code);
-	dig = va_arg(argument, int);
-	switch (error_code)
-	{
-		case 10:
-			fprintf(stderr, "L%d: can't pchar, value out of range\n", dig);
-			break;
-		case 11:
-			fprintf(stderr, "L%d: can't pchar, stack empty\n", dig);
-			break;
-		default:
-			break;
-	}
-	f_stack();
-	exit(EXIT_FAILURE);
+                buffer[nread++] = c;
+                if (c == '\n')
+                        break;
+        }
+        buffer[nread] = '\0';
+        *content = buffer;
+        *size = bufsize;
+        return (nread);
 }
