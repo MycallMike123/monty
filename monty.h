2589863f54920376cc4a37283cedd80cd8116528
Michael Mwangi <michaelmichire2016@gmail.com>
2023-12-15 07:10:53 -0800

    The mai header file for the project

--- monty.h
+++ monty.h
@@ -6,7 +6,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
-#include <stdarg.h>
 #include <unistd.h>
 #include <string.h>
 #include <ctype.h>
@@ -28,6 +27,7 @@ typedef struct stack_s
 	struct stack_s *next;
 } stack_t;
 
+
 /**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
@@ -43,35 +43,46 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-extern stack_t *first;
-typedef void (*_op)(stack_t **, unsigned int);
+/**
+ * struct line_s - variables -args, file, line content
+ * @arg: value
+ * @file: pointer to monty file
+ * @cont: line content
+ * @flag_c: flag change stack <-> queue
+ * Description: carries values through the program
+ */
+typedef struct line_s
+{
+	char *arg;
+	FILE *file;
+	char *cont;
+	int flag_c;
+}  line_t;
 
-void f_call(_op, char *, char *, int, int);
-void f_open(char *file_name);
-void f_stack(void);
-void error_(int error_code, ...);
-void _error(int error_code, ...);
-void str_error(int error_code, ...);
-void add(stack_t **, unsigned int);
-void handle_nop(stack_t **, unsigned int);
-void sub(stack_t **, unsigned int);
-void _div(stack_t **, unsigned int);
-void mul(stack_t **, unsigned int);
-void mod(stack_t **, unsigned int);
-void pchar(stack_t **, unsigned int);
-void pstr(stack_t **, unsigned int);
-void rotl(stack_t **, unsigned int);
-void rotr(stack_t **, unsigned int);
-stack_t *handle_push(int n);
-void handle_pint(stack_t **, unsigned int);
-void handle_pop(stack_t **, unsigned int);
-void handle_swap(stack_t **, unsigned int);
-void _stack(stack_t **, unsigned int);
-int _queue(char *buffer, int line_number, int format);
-void _addqueue(stack_t **, unsigned int);
-void _addnode(stack_t **, unsigned int);
-void rd_file(FILE *);
-void exec(char *, char *, int, int);
+line_t line = {NULL, NULL, NULL, 0};
+
+int exec(char *cont, stack_t **stack, unsigned int counter, FILE *file);
+void f_stack(stack_t *head);
+void add(stack_t **head, unsigned int counter);
+void handle_nop(stack_t **head, unsigned int counter);
+void sub(stack_t **head, unsigned int counter);
+void _div(stack_t **head, unsigned int counter);
+void mul(stack_t **head, unsigned int counter);
+void mod(stack_t **head, unsigned int counter);
+void pchar(stack_t **head, unsigned int counter);
+void pstr(stack_t **head, unsigned int counter);
+void rotl(stack_t **head, __attribute__((unused)) unsigned int counter);
+void rotr(stack_t **head, __attribute__((unused)) unsigned int counter);
+void handle_push(stack_t **head, unsigned int counter);
+void handle_pall(stack_t **head, unsigned int counter);
+void handle_pint(stack_t **head, unsigned int counter);
+void handle_pop(stack_t **head, unsigned int counter);
+void handle_swap(stack_t **head, unsigned int counter);
+void _stack(stack_t **head, unsigned int counter);
+void _queue(stack_t **head, unsigned int counter);
+void _addqueue(stack_t **head, int n);
+void _addnode(stack_t **head, int n);
 ssize_t custom_getline(char **content, size_t *size, FILE *file);
+
 
 #endif
